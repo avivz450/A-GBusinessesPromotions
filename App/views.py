f59a0fcf3b5c580c78985e21f7be45f827aeead2
Aviv Zafrani <avivz450@gmail.com>
2021-05-24 16:11:13 +0300

    Modified model migrations

--- App/views.py
+++ App/views.py
@@ -1,3 +1,10 @@
-# from django.shortcuts import render
+from django.shortcuts import render
+
 
 # Create your views here.
+def landingpage(request):
+    return render(request, 'home/landingpage.html')
+
+
+def base(request):
+    return render(request, 'home/base.html')
