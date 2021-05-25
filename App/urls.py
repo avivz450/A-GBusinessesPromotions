0d7a1b87cfcf12dd465be8f252f130594e254519
Aviv Zafrani <avivz450@gmail.com>
2021-05-25 18:00:27 +0300

    Added logout url

--- App/urls.py
+++ App/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('base/', views.base, name='base'),
     path('', views.landingpage, name='landingpage'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
