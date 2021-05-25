41acacb3209aea9d7919c371a916931bc6e82906
Gideon Shachar <gideonshachar@gmail.com>
2021-05-25 19:49:37 +0300

    Edit login to work with DB

--- App/urls.py
+++ App/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('base/', views.base, name='base'),
     path('', views.landingpage, name='landingpage'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('login/', views.login, name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='home/login.html'), name='login'),
 ]
