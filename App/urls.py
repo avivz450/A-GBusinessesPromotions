ee6c6292dfbbd9a4439668a80c38b649ba3fa325
Gideon Shachar <gideonshachar@gmail.com>
2021-05-25 18:23:01 +0300

    Edited login page

--- App/urls.py
+++ App/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('base/', views.base, name='base'),
     path('', views.landingpage, name='landingpage'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', views.login, name='login'),
 ]
