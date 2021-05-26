4b280a607ba2af7fafad0b49a55cb7d55ee2b733
Aviv Zafrani <avivz450@gmail.com>
2021-05-26 17:04:34 +0300

    Added sign up page

--- App/urls.py
+++ App/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.landingpage, name='landingpage'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('login/', auth_views.LoginView.as_view(template_name='home/login.html'), name='login'),
+    path('signup/', views.signup, name='signup'),
 ]
