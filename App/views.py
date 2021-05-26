fe9e2566786407b39d7846dc166b1552d152f5ed
Gideon Shachar <gideonshachar@gmail.com>
2021-05-26 20:19:22 +0300

    Edited sales page

--- App/views.py
+++ App/views.py
@@ -1,10 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Sale, Profile
+from .forms import SignUpForm
+from django.contrib.auth import login, authenticate
 
 
 # Create your views here.
 def landingpage(request):
-    return render(request, 'home/landingpage.html')
+    return render(request, 'home/landingpage.html', {'title': 'Welcome!'})
 
 
 def base(request):
     return render(request, 'home/base.html')
+
+
+def businesses(request):
+    return render(request, 'home/businesses.html')
+
+
+def sales(request):
+    if request.method == 'GET':
+        context = {
+            "sales": Sale.objects.all()
+        }
+    return render(request, 'home/sales.html', context)
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            raw_password = form.cleaned_data.get('password1')
+            user.save()
+            profile = Profile(user=user)
+            user = authenticate(username=user.username, password=raw_password)
+            profile.save()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = SignUpForm()
+    return render(request, 'home/signup.html', {'form': form, 'title': 'Sign Up'})
