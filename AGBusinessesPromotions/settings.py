304af96cb03698a09b69bcde114f9bc8303b6c05
Gideon Shachar <gideonshachar@gmail.com>
2021-05-28 14:08:07 +0300

    Fixing images in the sales page

--- AGBusinessesPromotions/settings.py
+++ AGBusinessesPromotions/settings.py
@@ -131,8 +131,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-IMAGES_ROOT = os.path.join(BASE_DIR, 'images')
-IMAGES_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '')
+MEDIA_URL = '//'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
