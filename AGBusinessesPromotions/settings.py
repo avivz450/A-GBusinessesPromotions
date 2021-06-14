71c12498c14225c6302f4c41dc7008067af2cc97
Aviv Zafrani <avivz450@gmail.com>
2021-06-14 18:58:02 +0300

    Add info messages

--- AGBusinessesPromotions/settings.py
+++ AGBusinessesPromotions/settings.py
@@ -143,4 +143,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
-MESSAGE_TAGS = {messages.ERROR: "danger"}
+MESSAGE_TAGS = {messages.ERROR: "danger", messages.INFO: "warning"}
