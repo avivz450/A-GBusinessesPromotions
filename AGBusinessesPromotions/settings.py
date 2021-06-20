6da2be6c6355987b47f7c4aba358496f4593c576
Gideon Shachar <58482256+gideonsh@users.noreply.github.com>
2021-06-20 23:31:53 +0300

    Update settings.py
    change in line 31: ALLOWED_HOSTS[] => ALLOWED_HOSTS["*"]
    to allowed an aws ec2 instance to run the app

--- AGBusinessesPromotions/settings.py
+++ AGBusinessesPromotions/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "django-insecure-8=tos27f4c)^5$#3v5&w!a913dvjlsw@bh@rzvcgd_)v-a-7y1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
