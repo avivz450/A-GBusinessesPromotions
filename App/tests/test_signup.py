f306ae145dfcdacd78687939128bbfc72dd196ed
Aviv Zafrani <avivz450@gmail.com>
2021-07-14 17:09:26 +0300

    Fixed login, logout, signup tests

--- App/tests/test_signup.py
+++ App/tests/test_signup.py
@@ -8,8 +8,8 @@ from django.urls import reverse
 
 @pytest.mark.django_db
 class TestSignUp:
-    demo_webpage_url = reverse("websitepage", args=[1, "Website_Demo"])
-    demo_webpage_signup_url = reverse("signup", args=[1, "Website_Demo"])
+    demo_webpage_url = reverse("websitepage", args=[1, "Website Demo"])
+    demo_webpage_signup_url = reverse("signup", args=[1, "Website Demo"])
 
     @pytest.fixture
     def valid_signup_details(self):
